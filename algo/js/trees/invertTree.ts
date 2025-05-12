9c9739c32578059284b970d91c111454a6adc573
Vitalii <vitalii.code@gmail.com>
2025-05-12 18:20:40 -0500

    invertTree

---
+++ algo/js/trees/invertTree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) return null;
+
+    let r = invertTree(root.right)
+    let l = invertTree(root.left)
+    root.left = r
+    root.right = l
+    return root;
+};
