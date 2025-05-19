9352126496101e28c756ac08978db06a77d98bb3
Vitalii <vasinkevych@gmail.com>
2025-05-19 15:19:30 -0500

    diameterOfBinaryTree

--- algo/js/trees/diameterBT.ts
+++ algo/js/trees/diameterBT.ts
@@ -14,10 +14,23 @@
 
 import { TreeNode } from "./TreeNode"
 
-function maxDepth(root: TreeNode | null): number {
-    if (!root) return 0
-    let left = maxDepth(root.left)
-    let right = maxDepth(root.right)
 
-    return Math.max(left, right) +1
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let longestTotal = 0
+
+    let dfs = (root: TreeNode | null) => {
+        if (!root) return 0
+
+        let left = dfs(root.left)
+        let right = dfs(root.right)
+
+        longestTotal = Math.max(longestTotal, left + right)
+
+        return Math.max(left, right)+1
+    }
+
+    dfs(root)
+
+    return longestTotal
+
 };
--- algo/js/trees/sortedArrayToBST.ts
+++ algo/js/trees/sortedArrayToBST.ts
@@ -3,16 +3,7 @@
 
  */
 
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.left = (left === undefined ? null : left)
-        this.right = (right === undefined ? null : right)
-    }
-}
+import { TreeNode } from "./TreeNode"
 
 function sortedArrayToBST(nums: number[]): TreeNode | null {
     let helper = (start, end) => {
