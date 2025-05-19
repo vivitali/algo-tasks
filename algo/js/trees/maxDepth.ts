68509db1668b8aa91a17c34aa752156de580d99d
Vitalii <vasinkevych@gmail.com>
2025-05-19 15:04:35 -0500

    UPDATE

---
+++ algo/js/trees/maxDepth.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) return 0
+    let left = maxDepth(root.left)
+    let right = maxDepth(root.right)
+
+    return Math.max(left, right) +1
+};
